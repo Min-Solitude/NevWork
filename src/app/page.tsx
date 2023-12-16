5b8b678914c2a4f485fc53ca3652dbc77cb9842c
Min solitude <121474260+Min-Solitude@users.noreply.github.com>
2023-12-16 10:44:43 +0700

    uodate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-import TabYoutube from '@/components/customs/TabYoutube';
 import type { Metadata } from 'next';
-import dynamic from 'next/dynamic';
 import MainLayout from "./(mainLayout)/layout";
+import dynamic from 'next/dynamic';
+import Links from '@/components/customs/Link';
+import Note from '@/components/customs/Note';
+import TabYoutube from '@/components/customs/TabYoutube';
+import Greeting from '@/components/customs/Greeting';
 
 export const metadata: Metadata = {
   title: {
@@ -17,10 +20,10 @@ export default function HomePage() {
   return (
     <MainLayout >
       <div className="flex justify-center gap-8 items-start h-[80vh] w-full md:max-w-[90%] md:m-auto">
-        {/* <Greeting /> */}
+        <Greeting />
         <TabYoutube />
-        {/* <Note /> */}
-        {/* <Links /> */}
+        <Note />
+        <Links />
         <div className=' flex-1 max-w-[18rem]'>1</div>
         <div className="flex-1 h-full flex justify-center items-center">
           <ManagerBackground />
